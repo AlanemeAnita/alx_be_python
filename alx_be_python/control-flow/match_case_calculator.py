102fca64417d5006d1b8f04f0d1c8ef993140e96
Anita <anitaalaneme@gmail.com>
2025-07-20 11:11:58 +0100

    Add match_case_calculator.py for control flow task

--- alx_be_python/control-flow/match_case_calculator.py
+++ alx_be_python/control-flow/match_case_calculator.py
@@ -1,24 +1,18 @@
-# match_case_calculator.py
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
     case "+":
-        result = num1 + num2
-        print(f"The result is {result}.")
+        print(f"The result is {num1 + num2}")
     case "-":
-        result = num1 - num2
-        print(f"The result is {result}.")
+        print(f"The result is {num1 - num2}")
     case "*":
-        result = num1 * num2
-        print(f"The result is {result}.")
+        print(f"The result is {num1 * num2}")
     case "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            result = num1 / num2
-            print(f"The result is {result}.")
+            print(f"The result is {num1 / num2}")
     case _:
-        print("Invalid operation selected.")
+        print("Invalid operation")
