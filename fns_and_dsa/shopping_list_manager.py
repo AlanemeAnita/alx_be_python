7cc03fdce6e4f5aa8c6f4e86b5d1d1d44e9a75c1
Alaneme Anita <anitaalaneme@gmail.com>
2025-07-27 15:49:23 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-shopping_list = []
-
-def display_menu():
-    print("1. Add item")
-    print("2. Remove item")
-    print("3. View list")
-    print("4. Exit")
-
-display_menu()
-
-choice = int(input("Enter your choice: "))
-=======
 shopping_list = []  
 
 def display_menu():  
