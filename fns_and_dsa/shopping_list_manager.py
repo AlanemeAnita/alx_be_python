3bf2668e5af69d3b4ae5e5b0b2ff5d9ecf13b904
Alaneme Anita <anitaalaneme@gmail.com>
2025-07-27 15:33:22 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,54 +1,14 @@
-# shopping_list_manager.py
-
 shopping_list = []
 
 def display_menu():
-    print("\nShopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View list")
     print("4. Exit")
 
-def add_item():
-    item = input("Enter item to add: ")
-    if item:
-        shopping_list.append(item)
-        print(f"{item} has been added to your list.")
-    else:
-        print("No item entered.")
+display_menu()
 
-def remove_item():
-    item = input("Enter item to remove: ")
-    if item in shopping_list:
-        shopping_list.remove(item)
-        print(f"{item} has been removed from your list.")
-    else:
-        print(f"{item} is not in your list.")
-
-def view_list():
-    if shopping_list:
-        print("\nYour Shopping List:")
-        for i, item in enumerate(shopping_list, 1):
-            print(f"{i}. {item}")
-    else:
-        print("Your shopping list is empty.")
-
-while True:
-    display_menu()  # <- Required by checker
-    try:
-        choice = int(input("Enter your choice: "))  # <- Input as number
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-        continue
-
-    if choice == 1:
-        add_item()
-    elif choice == 2:
-        remove_item()
-    elif choice == 3:
-        view_list()
-    elif choice == 4:
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid choice. Please try again.")
+try:
+    choice = int(input("Enter your choice: "))
+except ValueError:
+    print("Invalid input")
