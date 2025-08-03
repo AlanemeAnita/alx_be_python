0fe4bc1dd5742a478cc410974817fa2184631864
Anita <anitaalaneme@gmail.com>
2025-08-03 12:45:46 +0100

    Add Library Management System project

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,57 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False  # already checked out
+
+    def return_book(self):
+        """Mark the book as returned."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False  # book was not checked out
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # private list of Book objects
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return f"'{title}' has been checked out."
+        return f"Sorry, '{title}' is not available."
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return f"'{title}' has been returned."
+        return f"'{title}' was not checked out."
+
+    def list_available_books(self):
+        """List all available books."""
+        available_books = [f"{book.title} by {book.author}" for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(book)
+        else:
+            print("No books available.")
